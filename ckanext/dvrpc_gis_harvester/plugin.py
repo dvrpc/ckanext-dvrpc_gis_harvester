64ba1f5ff61081591375270962b4da0840eda729
Kris Warner <kdwarn@protonmail.com>
2021-07-17 09:44:43 -0400

    Apply black formatting

--- ckanext/dvrpc_gis_harvester/plugin.py
+++ ckanext/dvrpc_gis_harvester/plugin.py
@@ -8,7 +8,6 @@ class DvrpcGisHarvesterPlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'dvrpc_gis_harvester')
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("fanstatic", "dvrpc_gis_harvester")
