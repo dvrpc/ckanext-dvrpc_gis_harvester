344c5133fac860c1820046232114d348c3c1b791
Kris Warner <kdwarn@protonmail.com>
2021-07-17 02:54:38 -0400

    Change category to GIS

--- ckanext/dvrpc_gis_harvester/profiles.py
+++ ckanext/dvrpc_gis_harvester/profiles.py
@@ -1,20 +1,22 @@
 from ckanext.dcat.profiles import RDFProfile
 
+
 class GISProfile(RDFProfile):
-    '''
+    """
     An RDF profile based on the DCAT-AP for data portals in Europe
 
     More information and specification:
 
     https://joinup.ec.europa.eu/asset/dcat_application_profile
 
-    '''
+    """
+
     def parse_dataset(self, dataset_dict, dataset_ref):
-        dataset_dict['staff_contact'] = "Sean Lawrence"
-        dataset_dict['staff_contact_email'] = "slawrence@dvrpc.org"
-        dataset_dict['category'] = "Highway"
-        dataset_dict['posting_frequency'] = "as_needed"
-        dataset_dict['use_limitations'] = "unrestricted_without_agreement"
-        dataset_dict['agency_owner'] = "dvrpc"
+        dataset_dict["staff_contact"] = "Sean Lawrence"
+        dataset_dict["staff_contact_email"] = "slawrence@dvrpc.org"
+        dataset_dict["category"] = "GIS"
+        dataset_dict["posting_frequency"] = "as_needed"
+        dataset_dict["use_limitations"] = "unrestricted_without_agreement"
+        dataset_dict["agency_owner"] = "dvrpc"
 
         return dataset_dict
