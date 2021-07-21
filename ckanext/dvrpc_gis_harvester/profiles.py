dc57e4939b0af24b50e3ff16aca2c41420a4c517
Kris Warner <kdwarn@protonmail.com>
2021-07-21 21:53:03 +0000

    Include use_limitations (accessLevel) in dataset

--- ckanext/dvrpc_gis_harvester/profiles.py
+++ ckanext/dvrpc_gis_harvester/profiles.py
@@ -1,18 +1,19 @@
 import logging
 
+from rdflib.namespace import Namespace
+from rdflib.namespace import RDF
+
 from ckanext.dcat.profiles import RDFProfile
 
+DCT = Namespace("http://purl.org/dc/terms/")
+pod = Namespace("https://project-open-data.cio.gov/v1.1/schema/")
 log = logging.getLogger(__name__)
 
 
 class GISProfile(RDFProfile):
     """
-    An RDF profile based on the DCAT-AP for data portals in Europe
-
-    More information and specification:
-
-    https://joinup.ec.europa.eu/asset/dcat_application_profile
-
+    An RDF profile for the Delaware Valley Regional Planning Commission's
+    GIS data catalog.
     """
 
     def parse_dataset(self, dataset_dict, dataset_ref):
@@ -22,6 +23,7 @@ class GISProfile(RDFProfile):
         dataset_dict["posting_frequency"] = "as_needed"
         dataset_dict["agency_owner"] = "dvrpc"
         dataset_dict["tags"] = []
+
         if "notes" not in dataset_dict:
             dataset_dict["notes"] = "No description"
 
@@ -34,7 +36,22 @@ class GISProfile(RDFProfile):
                     updated_resources.append(resource)
             dataset_dict["resources"] = updated_resources
 
-        # this one still needs work
-        dataset_dict["use_limitations"] = "unrestricted_without_agreement"
+        # need to reach into the rdf graph to get the "accessLevel" field.
+        # first loop over all the datasets, and for the this dataset (id'd by dataset_ref),
+        # pull the field out of the "triple" (subject, predicate, object) that it's in.
+        # see <https://rdflib.readthedocs.io/en/stable/gettingstarted.html> for more info
+
+        # there may be a more efficient way, but for now this works
+
+        for s, p, o, g in self.g.quads((None, RDF.type, None, None)):
+            if s == dataset_ref:
+                for subj, pred, obj in g:
+                    if "accessLevel" in pred:
+                        if str(obj) == "public":
+                            dataset_dict["use_limitations"] = "unrestricted_without_agreement"
+                        elif str(obj) == "org":
+                            dataset_dict["use_limitations"] = "restricted_all_staff"
+
+                break  # no need to continue further in the loop once we have this
 
         return dataset_dict
